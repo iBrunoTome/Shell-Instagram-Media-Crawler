b6e147059d72e7c5045e42dd7fc99ce617736227
Bruno Tomé <ibrunotome@gmail.com>
2016-09-12 08:42:08 -0300

    Comments improved

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-function jsonval {
-    temp=`cat $username'.txt' | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+# Função para parsear o JSON de acordo com o atributo requisitado
+function parseJSON {
+    temp=`cat $username'.txt' | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $imageType`
     echo ${temp##*|}
 }
 
@@ -14,6 +15,7 @@ else # Preenche o usuário como 1º parâmetro
 	username=$(echo $1 | sed 's/@//g')
 fi
 
+# Se o usuário existir realiza o wget, se não existir, remove arquivos e termina a execução
 if curl -sSf 'https://www.instagram.com/'$username'/media/' > $username'.txt'; then
 	rm -rf ./$username
     mkdir $username
@@ -22,8 +24,8 @@ if curl -sSf 'https://www.instagram.com/'$username'/media/' > $username'.txt'; t
     mkdir thumbnail
     mkdir standard_resolution
     cd ../
-    prop='low_resolution'
-	jsonval
+    imageType='low_resolution'
+	parseJSON
 else
 	rm -rf ./$username'.txt'
 	echo 'Usuário inválido'
