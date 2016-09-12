6de98ffd5f11db144d103eaffc30943e9480a1c1
Bruno Tomé <ibrunotome@gmail.com>
2016-09-11 21:26:33 -0300

    Implemented a function to parse json

--- wrapper.sh
+++ wrapper.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function jsonval {
+    temp=`cat $username'.txt' | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+    echo ${temp##*|}
+}
+
 # Checa se não existe parâmetros, se não houver, pede a entrada via teclado
 if [ $# -eq 0 ]; then
 	echo -n 'Digite seu @usuario no Instagram: '
@@ -16,6 +21,9 @@ if curl -sSf 'https://www.instagram.com/'$username'/media/' > $username'.txt'; t
     mkdir low_resolution
     mkdir thumbnail
     mkdir standard_resolution
+    cd ../
+    prop='low_resolution'
+	jsonval
 else
 	rm -rf ./$username'.txt'
 	echo 'Usuário inválido'
